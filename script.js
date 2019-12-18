a44fb1143b4092a7a90f49357d32aa8fb819feb5
Manuel Ramirez <manuelramirez1411@gmail.com>
2019-12-18 17:05:43 -0500

    mejoras

--- script.js
+++ script.js
@@ -1,34 +1,15 @@
 var UPPER = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var number = [0,1,2,3,4,5,6,7,8,9];
-var special = [ "!","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\ ","]","^","_","{","|,","}","~"]
+var special = [ "!","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","{","|",",","}","~"]
 var generateBtn = document.querySelector ("#generate");
 var Passwordlenght = prompt("welcome to the password generator, how many characters do you want?");
 var UPPERdata = confirm("do you want upper case?");
 var lowerData = confirm("do you want lower case");
 var numberData = confirm("do you want numbers");
 var symbolData= confirm("do you want special characteres?");
-var MAYUSCULA = getRamdomUPPER();
-var minuscula = getRamdomlower();
-var numero = getRamdomnumber();
-var especial = getRamdomspecial();
-var mode1 = MAYUSCULA + minuscula + numero + especial+MAYUSCULA + minuscula + numero + especial;
-var mode2 = MAYUSCULA + minuscula + numero + MAYUSCULA + minuscula + numero; 
-var mode3 = MAYUSCULA + minuscula + especial + MAYUSCULA + minuscula + especial;;
-var mode4 = MAYUSCULA + numero + especial+ MAYUSCULA + numero + especial ;
-var mode5 = minuscula + numero + especial+minuscula + numero + especial;
-var mode6 = MAYUSCULA + minuscula + MAYUSCULA + minuscula;                        
-var mode7 = MAYUSCULA + numero + MAYUSCULA + numero;                               ;
-var mode8 = MAYUSCULA + especial+ MAYUSCULA + especial;
-var mode9 = minuscula + numero + minuscula + numero;
-var mode10 = minuscula + especial + minuscula + especial;
-var mode11 = numero + especial + numero + especial;
-var mode12 = MAYUSCULA+ MAYUSCULA+ MAYUSCULA+ MAYUSCULA ;
-var mode13 = minuscula+ minuscula+ minuscula+ minuscula ;
-var mode14 = numero + numero+ numero+numero ;
-var mode15 = especial+ especial+ especial+ especial;
-var maxlenght= 128;
 var passwordf= "";
+var display = document.getElementById("password").placeholder;
 
 
 function getRamdomUPPER(){
@@ -70,7 +51,7 @@ generateBtn.addEventListener("click", function () {
         console.log(pnumero);
     
         
-  } else if (UPPERdata === true && lowerData === true && numberData === true && symbolData === false) {
+  } else if (UPPERdata === true  && lowerData === true  && numberData === true  && symbolData === false) {
   
     for (var i=0 ; i <= Passwordlenght; i++){
 
@@ -87,7 +68,7 @@ generateBtn.addEventListener("click", function () {
       
   
 
-  } else if ( UPPERdata === true && lowerData === true && numberData === false && symbolData === true) {
+  } else if (UPPERdata === true  && lowerData === true  && numberData === false && symbolData === true) {
   
     for (var i=0 ; i <= Passwordlenght; i++){
 
@@ -103,7 +84,7 @@ generateBtn.addEventListener("click", function () {
           console.log(pnumero);
       
             
-  } else if (UPPERdata === true && lowerData === false && numberData === true && symbolData === true) {
+  } else if (UPPERdata === true  && lowerData === false && numberData === true  && symbolData === true) {
       
     for (var i=0 ; i <= Passwordlenght; i++){
 
@@ -119,7 +100,7 @@ generateBtn.addEventListener("click", function () {
           console.log(pnumero);
       
       
-  } else if (UPPERdata === false && lowerData === true && numberData === true && symbolData === true) {
+  } else if (UPPERdata === false && lowerData === true  && numberData === true  && symbolData === true) {
     for (var i=0 ; i <= Passwordlenght; i++){
 
       var passwordf= passwordf+lower[Math.floor(Math.random()*lower.length)]+number[Math.floor(Math.random()*number.length)]+special[Math.floor(Math.random()*special.length)];
@@ -134,7 +115,7 @@ generateBtn.addEventListener("click", function () {
           console.log(pnumero);
       
     
-  } else if ( UPPERdata === true && lowerData === true && numberData === false && symbolData === false) {
+  } else if (UPPERdata === true  && lowerData === true  && numberData === false && symbolData === false) {
       
     for (var i=0 ; i <= Passwordlenght; i++){
 
@@ -150,7 +131,7 @@ generateBtn.addEventListener("click", function () {
           console.log(pnumero);
           
                 
-  } else if (UPPERdata === true && lowerData === false && numberData === true && symbolData === false) {
+  } else if (UPPERdata === true  && lowerData === false && numberData === true  && symbolData === false) {
           
     for (var i=0 ; i <= Passwordlenght; i++){
 
@@ -165,7 +146,7 @@ generateBtn.addEventListener("click", function () {
         
           console.log(pnumero);
           
-  } else if (UPPERdata === true && lowerData === false && numberData === false && symbolData === true) {
+  } else if (UPPERdata === true  && lowerData === false && numberData === false && symbolData === true) {
       
     for (var i=0 ; i <= Passwordlenght; i++){
 
@@ -180,7 +161,7 @@ generateBtn.addEventListener("click", function () {
         
           console.log(pnumero);
           
-  } else if (UPPERdata === false && lowerData === true && numberData === true && symbolData === false) {
+  } else if (UPPERdata === false && lowerData === true  && numberData === true  && symbolData === false) {
       
     for (var i=0 ; i <= Passwordlenght; i++){
 
@@ -195,7 +176,7 @@ generateBtn.addEventListener("click", function () {
         
           console.log(pnumero);
         
-  } else if ( UPPERdata === false && lowerData === true && numberData === false && symbolData === true) {
+  } else if (UPPERdata === false && lowerData === true  && numberData === false && symbolData === true) {
           
     for (var i=0 ; i <= Passwordlenght; i++){
 
@@ -210,7 +191,7 @@ generateBtn.addEventListener("click", function () {
         
           console.log(pnumero);
                     
-  } else if (UPPERdata === false && lowerData === false && numberData === true && symbolData === true) {
+  } else if (UPPERdata === false && lowerData === false && numberData === true  && symbolData === true) {
               
     for (var i=0 ; i <= Passwordlenght; i++){
 
@@ -221,7 +202,7 @@ generateBtn.addEventListener("click", function () {
           var pnumero =  passwordf.substring(0, Passwordlenght);
         
           console.log(pnumero);
-  } else if (UPPERdata === true && lowerData === false && numberData === false && symbolData === false) {
+  } else if (UPPERdata === true  && lowerData === false && numberData === false && symbolData === false) {
         for (var i=0 ; i <= Passwordlenght; i++){
 
           var passwordf= passwordf+(UPPER[Math.floor(Math.random()*UPPER.length)]);
@@ -232,7 +213,7 @@ generateBtn.addEventListener("click", function () {
             
               console.log(pnumero);
               
-  } else if (UPPERdata === false && lowerData === true && numberData === false && symbolData === false) {
+  } else if (UPPERdata === false && lowerData === true  && numberData === false && symbolData === false) {
           
     for (var i=0 ; i <= Passwordlenght; i++){
 
@@ -247,7 +228,7 @@ generateBtn.addEventListener("click", function () {
         
           console.log(pnumero);
             
-  } else if ( UPPERdata === false && lowerData === false && numberData === true && symbolData === false) {
+  } else if (UPPERdata === false && lowerData === false && numberData === true  && symbolData === false) {
               
     for (var i=0 ; i <= Passwordlenght; i++){
 
@@ -259,7 +240,7 @@ generateBtn.addEventListener("click", function () {
         
           console.log(pnumero);
          
-  } else if  (UPPERdata === false && lowerData === false && numberData === false && symbolData === true) {
+  } else if (UPPERdata === false && lowerData === false && numberData === false && symbolData === true) {
                   
     for (var i=0 ; i <= Passwordlenght; i++){
 
@@ -276,7 +257,8 @@ generateBtn.addEventListener("click", function () {
   } else {
 
       alert("no password");
-      console.log(getmode12());
+      console.log("no password");
+      var display=12345;
   };
 
   
