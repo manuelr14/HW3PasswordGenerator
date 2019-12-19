e718349c32223a86d3c218e9934eafe29708bda9
Manuel Ramirez <manuelramirez1411@gmail.com>
2019-12-18 22:34:35 -0500

    final no copyc

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q"
 var number = [0,1,2,3,4,5,6,7,8,9];
 var special = [ "!","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","{","|",",","}","~"]
 var generateBtn = document.querySelector ("#generate");
+var CopyBtn = document.querySelector ("#generate");
 var display = document.querySelector("#password");
 
 
@@ -31,14 +32,14 @@ function getRamdomspecial(){
 
   generateBtn.addEventListener("click", function () {
 
-    var Passwordlenght = prompt("welcome to the password generator, pick a characther betwen 8 -1 28?");
+    var Passwordlenght = prompt("welcome to the password generator, pick a characther betwen 8 -128");
     
     if( Passwordlenght >= 8 && Passwordlenght <= 128) {
 
       var UPPERdata = confirm("do you want upper case?");
 
-        var lowerData = confirm("do you want lower case");
-        var numberData = confirm("do you want numbers");
+        var lowerData = confirm("do you want lower case?");
+        var numberData = confirm("do you want numbers?");
         var symbolData= confirm("do you want special characteres?");
 
     
@@ -249,6 +250,7 @@ function getRamdomspecial(){
               var pnumero =  passwordf.substring(0, Passwordlenght);
             
               console.log(pnumero);
+              display.textContent=pnumero; 
                 
       } else if (UPPERdata === false && lowerData === false && numberData === true  && symbolData === false) {
                   
@@ -261,6 +263,7 @@ function getRamdomspecial(){
               var pnumero =  passwordf.substring(0, Passwordlenght);
             
               console.log(pnumero);
+              display.textContent=pnumero; 
              
       } else if (UPPERdata === false && lowerData === false && numberData === false && symbolData === true) {
                       
@@ -269,19 +272,19 @@ function getRamdomspecial(){
           var passwordf= passwordf+special[Math.floor(Math.random()*special.length)];
     
         }
-             
-              
-           
             console.log(passwordf)
               var pnumero =  passwordf.substring(0, Passwordlenght);
             
               console.log(pnumero);
+              display.textContent=pnumero; 
       } else {
     
-          alert("you have to check at least one option!");
+          alert("you have to pick between 8 -128 chacters and check at least one character type!");
           console.log("no password");
           
       };
- 
   }); 
-                        
+  
+  
+ 
+
